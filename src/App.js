a1d5581018cc2d0ede7039929e23f9b6e42e0f82
Vivek <kapalvivek@gmail.com>
2024-06-19 21:28:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [displayValue, setdisplayValue] = useState("");
+
+  function Oprations(value) {
+    let Oprands = ["+", "-", "*", "/"];
+    let result;
+
+    try {
+      let Oprator = null;
+      for (let i = 0; i < displayValue.length; i++) {
+        if (Oprands.includes(displayValue[i])) {
+          Oprator = displayValue[i];
+          break;
+        }
+      }
+
+      if (!Oprator) {
+        setdisplayValue(parseFloat(displayValue).toString());
+        return;
+      }
+
+      const [value1, value2] = displayValue.split(Oprator).map(parseFloat);
+
+      switch (Oprator) {
+        case "+":
+          result = (value1 + value2).toString();
+          break;
+        case "-":
+          result = (value1 - value2).toString();
+          break;
+        case "*":
+          result = (value1 * value2).toString();
+          break;
+        case "/":
+          result = (value1 / value2).toString();
+          break;
+        default:
+          throw new Error("Invalid value");
+      }
+
+      setdisplayValue(result);
+    } catch (e) {
+      setdisplayValue(e);
+    }
+  }
+
+  const ButtonClicked = (value) => {
+    if (value === "AC") {
+      setdisplayValue("");
+    } else if (value === "Backspace") {
+      setdisplayValue(displayValue.slice(0, -1));
+    } else if (value === "=") {
+      Oprations(displayValue);
+    } else {
+      setdisplayValue((previousValue) => previousValue + value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="calculator">
+      <input type="text" className="calculator-screen" value={displayValue} />
+      <div className="calculator-keys">
+        <button
+          onClick={() => ButtonClicked("+")}
+          className="operator"
+          value="+"
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </button>
+        <button
+          onClick={() => ButtonClicked("-")}
+          className="operator"
+          value="-"
+        >
+          -
+        </button>
+        <button
+          onClick={() => ButtonClicked("*")}
+          className="operator"
+          value="*"
+        >
+          &times;
+        </button>
+        <button
+          onClick={() => ButtonClicked("/")}
+          className="operator"
+          value="/"
+        >
+          &divide;
+        </button>
+
+        <button onClick={() => ButtonClicked("7")} value="7">
+          7
+        </button>
+        <button onClick={() => ButtonClicked("8")} value="8">
+          8
+        </button>
+        <button onClick={() => ButtonClicked("9")} value="9">
+          9
+        </button>
+
+        <button onClick={() => ButtonClicked("4")} value="4">
+          4
+        </button>
+        <button onClick={() => ButtonClicked("5")} value="5">
+          5
+        </button>
+        <button onClick={() => ButtonClicked("6")} value="6">
+          6
+        </button>
+
+        <button onClick={() => ButtonClicked("1")} value="1">
+          1
+        </button>
+        <button onClick={() => ButtonClicked("2")} value="2">
+          2
+        </button>
+        <button onClick={() => ButtonClicked("3")} value="3">
+          3
+        </button>
+
+        <button onClick={() => ButtonClicked("0")} value="0">
+          0
+        </button>
+        <button
+          onClick={() => ButtonClicked(".")}
+          className="decimal"
+          value="."
+        >
+          .
+        </button>
+        <button
+          onClick={() => ButtonClicked("Backspace")}
+          className="equal-sign operator"
+          value="Backspace"
+        >
+          Backspace
+        </button>
+        <button
+          onClick={() => ButtonClicked("AC")}
+          className="all-clear"
+          value="all-clear"
+        >
+          AC
+        </button>
+
+        <button
+          onClick={() => ButtonClicked("=")}
+          className="equal-sign operator"
+          value="="
+        >
+          =
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
